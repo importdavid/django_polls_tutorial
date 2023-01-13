8a10686c0be4f10319e293612763fed32f51c0b8
David Wilkins <wilkidav@gmail.com>
2023-01-12 21:16:15 -0500

    Changed settings.py to include STATIC_ROOT for dev/prod

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,6 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# for development
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# for production
+#STATIC_ROOT = "/var/www/scobydew.com/static"
+# Not sure if I need this yet
+#STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
